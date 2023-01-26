dac10dd6cd018b49a89f526b2254402954a9ea3d
albotw <yann.trou@gmail.com>
2023-01-26 12:08:06 +0100

    Auto stash before merge of "main" and "origin/main"

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,7 +1,36 @@
+import org.apache.spark
 import org.apache.spark.sql.SparkSession
 
 object Test {
   def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder()
+      .appName("YelpETL")
+      //.config("spark.config.option", "some-value")
+      .getOrCreate();
+
+    val path_business = "D:\\Yann\\__DEV\\YelpETL\\M2BDIA-ID-Ressources\\dataset\\yelp_academic_dataset_business.json";
+    val path_checkin = "D:\\Yann\\__DEV\\YelpETL\\M2BDIA-ID-Ressources\\dataset\\yelp_academic_dataset_checkin.json";
+    val path_tip = "D:\\Yann\\__DEV\\YelpETL\\M2BDIA-ID-Ressources\\dataset\\yelp_academic_dataset_tip.csv";
+
+    val df_business = spark.read.json(path_business);
+    val df_checkin = spark.read.json(path_checkin)
+    val df_tip = spark.read.csv(path_tip)
+
+    df_business.show()
+    df_business.printSchema()
+    print(df_business.count())
+
+    df_checkin.show()
+    df_checkin.printSchema()
+    print(df_checkin.count())
+
+    df_tip.show()
+    df_tip.printSchema()
+    print(df_tip.count())
+  }
+
+  def test(): Unit = {
     val logFile = "D:\\Yann\\__DEV\\spark-3.3.1-bin-hadoop3\\README.md"
     val spark = SparkSession.builder.appName("Test app").getOrCreate()
     //spark.sparkContext.setLogLevel("WARN"); // réduire la verbose au strict minimum
